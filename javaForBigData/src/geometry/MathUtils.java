2d2ba9b91202b8c73e85a10ee3154daeb956f2d6
ecastive <ecastive@MAD-2FCH4M2.usersad.everis.int>
2018-10-30 09:09:34 +0100

    Rethinking the power function

--- javaForBigData/src/geometry/MathUtils.java
+++ javaForBigData/src/geometry/MathUtils.java
@@ -5,10 +5,16 @@ public class MathUtils {
 	public static float PI = 3.1416f;
 	
 	public static float power(float x, int n) {
+		if (n == 0) return 1;
+		else if (n > 0) return positivePower(x, n);
+		else return 1/positivePower(x, -n);
+	}
+
+	private static float positivePower(float x, int n) {
 		float result = 1;
 		int i = 0;
 		for(i=0; i<n; i++) {
-			result *=x;
+			result *= x;
 		}
 		return result;
 	}
